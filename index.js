d3c5d582271a67ef63611c65e5efdcc1eb2debda
Dobataa <dobatabg04@gmail.com>
2024-03-29 16:59:21 +0200

    create routes and views

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 const { PORT } = require('./config/config');
 const expressConfig = require('./config/express');
 const mondooseConfig = require('./config/mongoose');
+const routes = require('./routes');
 
 expressConfig(app);
 mondooseConfig();
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is runnig on port ${PORT}...`));
